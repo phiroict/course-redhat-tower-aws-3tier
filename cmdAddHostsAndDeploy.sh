4bcce34547e3086b369b065ad2b80ae9cf5a5ea7
pr1438 <philip.rodrigues@solnet.co.nz>
2018-03-13 10:59:36 +1300

    Dynamic inventory searching the jumphost as that has a jumping ip address.

--- cmdAddHostsAndDeploy.sh
+++ cmdAddHostsAndDeploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source ~/.aws/set-env.sh
-ansible-playbook --ask-vault-pass -vvvv -i aws_jump_host pb_aws_install_apps.yml
+ansible-playbook --ask-vault-pass -i aws_jump_host pb_aws_install_apps.yml
